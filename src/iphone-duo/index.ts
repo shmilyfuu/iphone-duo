46cd1f99e17c03e866e605d1bc581fa7316b3e88
shmilyfuu <47942250+shmilyfuu@users.noreply.github.com>
2026-09-11 11:45:18 +0800

    chore: export media type

--- src/iphone-duo/index.ts
+++ src/iphone-duo/index.ts
@@ -1,4 +1,4 @@
 export { FoldablePhone, FoldToggle, FoldScrubber, PhoneBackground, AppleCredit, useFoldablePhone } from './FoldablePhone'
 export type { FoldablePhoneProps } from './FoldablePhone'
 export { PhoneDevice } from './PhoneDevice'
-export type { PhoneDeviceProps } from './PhoneDevice'
+export type { PhoneDeviceProps, MediaKind } from './PhoneDevice'
